6eb75a35fd8837b34c5eb67d4efe168b7865c393
박준식 <akfldkskgorn@gmail.com>
2024-07-15 14:07:45 +0900

    🌐ci : react + vite + typescript + redux 프로젝트 설정

---
+++ frontend/src/main.tsx
@@ -0,0 +1,15 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./modules/index.ts";
+
+const store = createStore(rootReducer);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
