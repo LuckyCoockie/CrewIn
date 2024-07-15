b3ae7e88d4eecc9edee2c0a96fb43e3fbbd10700
박준식 <akfldkskgorn@gmail.com>
2024-07-16 00:26:51 +0900

    ➕feat : beautiful dnd 모듈화

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,9 @@
-import List from "./componetns/pages/List";
-import React from 'react';
-import CropperTest from './cropper/CropperTest';
-import ImageEditor from "./components/Html2canvasTest";
+import List from "./components/pages/MarkerList";
+import React from "react";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <CropperTest />
-      <ImageEditor />
       <List></List>
     </div>
   );
