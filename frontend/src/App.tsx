f292902fe953875c74c148e60213f1643c2985b3
권윤하 <ellen4421@naver.com>
2024-07-16 16:30:49 +0900

    ➕feat  : cropper & html2canvans 연결

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,10 @@
-import List from "./componetns/pages/List";
 import React from 'react';
-import CropperTest from './cropper/CropperTest';
-import ImageEditor from "./components/Html2canvasTest";
+import UploadImage from './components/pages/UploadImage';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <CropperTest />
-      <ImageEditor />
-      <List></List>
-    </div>
-  );
+    <UploadImage />
+  )
 };
 
 export default App;
