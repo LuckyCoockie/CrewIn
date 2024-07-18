dbd8ad343d9ea48a288eda41fbe687474dafad85
박효진 <jam_ty@naver.com>
2024-07-18 16:19:34 +0900

    ➕feat : Input Dropdown 타입 Atoms/Molecule 생성

---
+++ frontend/src/components/atoms/InputDropdownTypeComponent.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+type Option = {
+  label: string;
+  value: string;
+};
+
+type InputData = {
+  id: string;
+  options: Option[];
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  hasError?: boolean;
+  disabled?: boolean;
+};
+
+const InputDropdownTypeComponent = React.forwardRef<HTMLSelectElement, InputData>(
+  (props, ref) => {
+    return (
+      <select
+        id={props.id}
+        value={props.value}
+        onChange={props.onChange}
+        ref={ref}
+        className={`data-input border ${
+          props.hasError
+            ? "border-red-500 focus:border-red-500"
+            : "border-gray-300"
+        }`}
+        disabled={props.disabled}
+      >
+        <option value="">---</option>
+        {props.options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    );
+  }
+);
+
+export default InputDropdownTypeComponent;
