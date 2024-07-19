f81f08f24781a878e57aeaa72fc0c2de9c423284
박효진 <jam_ty@naver.com>
2024-07-19 17:08:06 +0900

    ➕feat : 제한 없는 텍스트 Input 타입 추가

---
+++ frontend/src/components/molecules/InputTextAreaNoLimitTypeMolecule.tsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import InputLabelComponent from "../atoms/Input/InputLabelComponent";
+import InputTextAreaTypeComponent from "../atoms/Input/InputTextAreaTypeComponent";
+
+type InputData = {
+  id: string;
+  title: string;
+  name: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  placeholder: string;
+  error?: string;
+  hasError?: boolean;
+};
+
+const InputTextAreaNoLimitTypeMolecule = React.forwardRef<
+  HTMLTextAreaElement,
+  InputData
+>((props, ref) => {
+  const [charCount, setCharCount] = useState(
+    props.value ? props.value.length : 0
+  );
+
+  useEffect(() => {
+    setCharCount(props.value ? props.value.length : 0);
+  }, [props.value]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    if (e.target.value.length <= 1000) {
+      setCharCount(e.target.value.length);
+      props.onChange(e);
+    } else {
+      e.target.value = e.target.value.slice(0, 1000);
+      setCharCount(1000);
+      props.onChange(e);
+    }
+  };
+
+  return (
+    <div className="mb-4 w-full">
+      <InputLabelComponent id={props.id} title={props.title} />
+      <InputTextAreaTypeComponent
+        placeholder={props.placeholder}
+        id={props.id}
+        name={props.name}
+        value={props.value}
+        ref={ref}
+        onChange={handleChange}
+        hasError={props.hasError}
+      />
+      {props.error ? (
+        <p className="ps-4 pt-1 text-sm font-light text-red-500">
+          {props.error}
+        </p>
+      ) : (
+        <p className="text-gray-500 text-end">{charCount}/1000 글자</p>
+      )}
+    </div>
+  );
+});
+
+export default InputTextAreaNoLimitTypeMolecule;
