9bc33e20a912b2367e8484ef1109eb4292043e87
박준식 <akfldkskgorn@gmail.com>
2024-07-19 17:33:15 +0900

    ➕feat : Login Api component 연결

--- frontend/src/components/organisms/LoginOrganism.tsx
+++ frontend/src/components/organisms/LoginOrganism.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm, Controller, SubmitHandler } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
@@ -6,6 +6,10 @@ import * as yup from "yup";
 import InputTextTypeMolecule from "../molecules/InputTextTypeMolecule";
 import InputPasswordTypeMolecule from "../molecules/InputPasswordTypeMolecule";
 import LargeDisableButton from "../atoms/Button/LargeDisableButton";
+
+type OwnProps = {
+  login: (value: FormValues) => Promise<void>;
+};
 
 // 유효성 검사 스키마 정의
 const schema = yup.object({
@@ -20,7 +24,7 @@ type FormValues = {
   password: string;
 };
 
-const LoginOrganism: React.FC = () => {
+const LoginOrganism: React.FC<OwnProps> = ({ login }: OwnProps) => {
   const {
     control,
     handleSubmit,
@@ -31,9 +35,16 @@ const LoginOrganism: React.FC = () => {
     mode: "onChange",
     defaultValues: {},
   });
+
+  const [isFailed, setIsFailed] = useState<boolean>(false);
+
   const onSubmit: SubmitHandler<FormValues> = (data) => {
-    console.log(data);
+    login(data).catch((error) => {
+      console.log(error);
+      setIsFailed(true);
+    });
   };
+
   return (
     <div className="mx-auto w-full max-w-[550px] pt-4 pb-10">
       <form onSubmit={handleSubmit(onSubmit)}>
@@ -72,6 +83,11 @@ const LoginOrganism: React.FC = () => {
           />
         </div>
         <div>
+          {isFailed ?? (
+            <p className="ps-4 pt-1 text-sm font-light text-red-500">
+              {"이메일 또는 비밀번호가 잘못되었습니다."}
+            </p>
+          )}
           <LargeDisableButton text="로그인" />
         </div>
       </form>
