add39af45b73db79aef45d91fab9e0569a7b2886
권오윤 <rdd2033@naver.com>
2024-07-21 13:39:39 +0900

    ➕feat : CORS 설정 추가

---
+++ backend/src/main/java/com/luckycookie/crewin/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.luckycookie.crewin.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${frontend.server.url}")
+    private String frontURL;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(frontURL) // 허용할 출처
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE") // 허용할 HTTP method
+                .allowCredentials(true) // 쿠키 인증 요청 허용
+                .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
+    }
+}
