4b2aac52d90a909055a94f848c1e23a5ed9a5566
박준식 <akfldkskgorn@gmail.com>
2024-07-23 00:07:13 +0900

    ➕feat : grid list 구현

---
+++ frontend/src/components/molecules/GridListMolecule.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export interface ItemComponentProps<T> {
+  index: number;
+  item: T;
+}
+
+export type OwnProps<T> = {
+  items: T[];
+  children: (props: ItemComponentProps<T>) => React.ReactElement<HTMLElement>;
+};
+
+const GridListComponent = <T,>({
+  items,
+  children: ItemComponent,
+}: OwnProps<T>) => {
+  return (
+    <div className="grid grid-cols-2 gap-2 md:gap-4">
+      {items.map((item, index) => (
+        <ItemComponent index={index} item={item} />
+      ))}
+    </div>
+  );
+};
+
+export default GridListComponent;
