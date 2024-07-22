439b196d708687c5be98284e305dd7509ba4a7cc
김민경 <org9899@naver.com>
2024-07-22 17:42:11 +0900

    ➕feat : Crew Create Notice Request 생성

--- backend/src/main/java/com/luckycookie/crewin/dto/CrewRequest.java
+++ backend/src/main/java/com/luckycookie/crewin/dto/CrewRequest.java
@@ -1,6 +1,8 @@
 package com.luckycookie.crewin.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.luckycookie.crewin.domain.PostImage;
+import com.luckycookie.crewin.domain.enums.PostType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,6 +10,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @NoArgsConstructor
 public class CrewRequest {
@@ -33,6 +36,18 @@ public class CrewRequest {
         @DateTimeFormat(pattern = "yyyy-MM-dd")
         @JsonFormat(pattern = "yyyy-MM-dd")
         private LocalDate crewBirth; // 크루 생일
+
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class CreateCrewNoticeRequest{
+        private Long crewId; // crewId
+        private String title; // 공지글 제목
+        private String content; // 공지글 내용
+        private List<String> noticeImages; // 공지 이미지 List
 
     }
 
