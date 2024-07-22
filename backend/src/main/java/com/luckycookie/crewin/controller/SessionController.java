f59901c4e8ebb2ee23407744c219fd936ce507ac
김민경 <org9899@naver.com>
2024-07-22 16:16:31 +0900

    🐛fix : BE/세션 등록 API 수정 (User 정보 추가)

--- backend/src/main/java/com/luckycookie/crewin/controller/SessionController.java
+++ backend/src/main/java/com/luckycookie/crewin/controller/SessionController.java
@@ -3,10 +3,12 @@ package com.luckycookie.crewin.controller;
 import com.luckycookie.crewin.dto.PostRequest;
 import com.luckycookie.crewin.dto.SessionRequest;
 import com.luckycookie.crewin.dto.base.BaseResponse;
+import com.luckycookie.crewin.security.dto.CustomUser;
 import com.luckycookie.crewin.service.SessionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +22,8 @@ public class SessionController {
     private final SessionService sessionService;
 
     @PostMapping()
-    public ResponseEntity<BaseResponse<Void>> createPost(@RequestBody SessionRequest.CreateSessionRequest createSessionRequest) {
-        sessionService.createSession(createSessionRequest);
+    public ResponseEntity<BaseResponse<Void>> createPost(@AuthenticationPrincipal CustomUser customUser, @RequestBody SessionRequest.CreateSessionRequest createSessionRequest) {
+        sessionService.createSession(createSessionRequest, customUser);
         return ResponseEntity.ok(BaseResponse.create(HttpStatus.OK.value(), "세션을 등록하는데 성공했습니다."));
     }
 
