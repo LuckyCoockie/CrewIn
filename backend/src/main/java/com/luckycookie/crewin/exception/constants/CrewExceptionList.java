7ea5f44ce1b4a239955adac39abd46a7a66bd156
김민경 <org9899@naver.com>
2024-07-22 17:43:47 +0900

    ➕feat : Crew 공지 등록 Postion Mismatch Exception 추가

--- backend/src/main/java/com/luckycookie/crewin/exception/constants/CrewExceptionList.java
+++ backend/src/main/java/com/luckycookie/crewin/exception/constants/CrewExceptionList.java
@@ -8,9 +8,11 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum CrewExceptionList {
     NOT_FOUND_CREW_ERROR("C0001", HttpStatus.NOT_FOUND, "크루를 찾는데 실패했습니다."),
-    CREW_LIST_ERROR("C0002", HttpStatus.BAD_REQUEST, "크루 목록을 불러오지 못했습니다.");
+    CREW_LIST_ERROR("C0002", HttpStatus.BAD_REQUEST, "크루 목록을 불러오지 못했습니다."),
+    CREW_POSITION_MISMATCH_ERROR("C0003", HttpStatus.BAD_REQUEST, "크루 공지는 PACER 이상만 가능합니다.");
 
     private final String errorCode;
     private final HttpStatus httpStatus;
     private final String message;
+
 }
