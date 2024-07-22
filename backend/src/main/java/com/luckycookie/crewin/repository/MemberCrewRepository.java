50f5f90573e6fdac605026e042f4ef3691a0d31c
김민경 <org9899@naver.com>
2024-07-22 17:42:46 +0900

    ➕feat : MemberCrewRepository Member Position 반환 Query 추가

--- backend/src/main/java/com/luckycookie/crewin/repository/MemberCrewRepository.java
+++ backend/src/main/java/com/luckycookie/crewin/repository/MemberCrewRepository.java
@@ -1,10 +1,18 @@
 package com.luckycookie.crewin.repository;
 
-import com.luckycookie.crewin.domain.Crew;
 import com.luckycookie.crewin.domain.Member;
 import com.luckycookie.crewin.domain.MemberCrew;
+import com.luckycookie.crewin.domain.enums.Position;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface MemberCrewRepository extends JpaRepository<MemberCrew, Long> {
     boolean existsByMemberAndIsJoinedTrue(Member member);
+
+    // 해당 Member 의 Position 반환
+    @Query("SELECT mc.position FROM MemberCrew mc WHERE mc.member = :member")
+    Optional<Position> findPositionByMember(@Param("member") Member member);
 }
