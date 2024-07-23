07b9e6194e3e85ce1117c99400d1795b6bf85e9d
권오윤 <rdd2033@naver.com>
2024-07-23 13:18:19 +0900

    🐛fix : OPTIONS CORS 허용

--- backend/src/main/java/com/luckycookie/crewin/config/WebConfig.java
+++ backend/src/main/java/com/luckycookie/crewin/config/WebConfig.java
@@ -16,14 +16,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(frontURL) // 허용할 출처
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE") // 허용할 HTTP method
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
 
         // 로컬 테스트 허용
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173") // 허용할 출처
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE") // 허용할 HTTP method
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
     }
