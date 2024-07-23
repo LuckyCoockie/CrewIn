02c0c37b33c50a6597a05dce230367ceac938890
권오윤 <rdd2033@naver.com>
2024-07-23 15:57:05 +0900

    🐛fix : 시큐리티 설정

--- backend/src/main/java/com/luckycookie/crewin/config/SecurityConfig.java
+++ backend/src/main/java/com/luckycookie/crewin/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import com.luckycookie.crewin.security.JwtAuthenticationEntryPoint;
 import com.luckycookie.crewin.security.JwtFilter;
 import com.luckycookie.crewin.security.util.TokenUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -18,10 +17,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.CorsUtils;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 import java.util.List;
@@ -35,9 +31,6 @@ public class SecurityConfig {
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
     private final MemberRepository memberRepository;
     private final TokenUtil tokenUtil;
-
-    @Value("${frontend.server.url}")
-    private String frontURL;
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -69,18 +62,5 @@ public class SecurityConfig {
                 });
 
         return http.build();
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", frontURL));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 }
