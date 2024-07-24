59b056c2439ff85fcc478569e91ebe4f16a22890
김민경 <org9899@naver.com>
2024-07-24 11:49:43 +0900

    🐛fix : Crew 포함 판단 Quey 문 수정

--- backend/src/main/java/com/luckycookie/crewin/repository/MemberCrewRepository.java
+++ backend/src/main/java/com/luckycookie/crewin/repository/MemberCrewRepository.java
@@ -7,11 +7,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MemberCrewRepository extends JpaRepository<MemberCrew, Long> {
-    boolean existsByMemberAndIsJoinedTrue(Member member);
+
+    @Query(value = "SELECT is_joined FROM membercrew WHERE member_id = :memberId AND crew_id = :crewId", nativeQuery = true)
+    Integer findIsJoinedByMemberIdAndCrewId(@Param("memberId") Long memberId, @Param("crewId") Long crewId);
+
+    List<Integer> existsByMemberAndIsJoinedTrue(Member member);
 
     // 해당 Member 의 Position 반환
     @Query("SELECT mc.position FROM MemberCrew mc WHERE mc.member = :member")
