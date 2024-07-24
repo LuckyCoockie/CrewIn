aa961ef87051a4df52b82be4a17ab740ae4c83ed
박효진 <jam_ty@naver.com>
2024-07-24 21:46:27 +0900

    ➕feat : (+)버튼 추가, 이미지 버튼 컴포넌트 생성

---
+++ frontend/src/components/atoms/Button/ImageButton.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+type Image = {
+  src: string;
+  alt: string;
+};
+
+const ImageButton: React.FC<Image> = ({ src, alt }) => {
+  const handleClickButton = () => {
+    console.log(`${alt}버튼 클릭`);
+  };
+  return (
+    <button
+      onClick={handleClickButton}
+      style={{
+        width: "50px",
+        height: "50px",
+        padding: 0,
+        border: "none",
+        background: "none",
+        cursor: "pointer",
+      }}
+    >
+      <img
+        src={src}
+        alt={alt}
+        style={{
+          width: "100%",
+          height: "100%",
+          borderRadius: "10px",
+          objectFit: "cover",
+        }}
+      />
+    </button>
+  );
+};
+
+export default ImageButton;
