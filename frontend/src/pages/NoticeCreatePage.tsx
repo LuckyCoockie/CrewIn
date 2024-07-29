1b945e9dbac0b5c5ff15d662beb6da3d91827a28
권윤하 <ellen4421@naver.com>
2024-07-29 14:41:36 +0900

    ➕feat : dropzone 컴포넌트화, 크루 공지글 작성 페이지 레이아웃

---
+++ frontend/src/pages/NoticeCreatePage.tsx
@@ -0,0 +1,9 @@
+// src/pages/NoticeCreatePage.tsx
+import React from "react";
+import NoticeCreateTemplate from "../components/templates/crew/NoticeCreateTemplate";
+
+const NoticeCreatePage: React.FC = () => {
+  return <NoticeCreateTemplate />;
+};
+
+export default NoticeCreatePage;
