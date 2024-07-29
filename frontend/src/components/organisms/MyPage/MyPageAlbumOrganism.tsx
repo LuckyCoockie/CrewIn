ebf1b3de923530b4f93f65f5d6bb95010ee2da24
박효진 <jam_ty@naver.com>
2024-07-29 12:21:47 +0900

    🔧chore : npm run build 오류 임시 조치

--- frontend/src/components/organisms/MyPage/MyPageAlbumOrganism.tsx
+++ frontend/src/components/organisms/MyPage/MyPageAlbumOrganism.tsx
@@ -1,11 +1,20 @@
 import React from "react";
-
-type PhotosProps = {
-  // postId: number;
-  photos: string[];
-};
+import logo from "../../../assets/images/crewinlogo.png";
 
 const MyPageAlbumOrganism: React.FC = () => {
+  const photos = [
+    logo,
+    logo,
+    logo,
+    logo,
+    logo,
+    logo,
+    logo,
+    logo,
+    logo,
+    logo,
+    logo,
+  ];
   return (
     <>
       {photos.map((photo, index) => (
