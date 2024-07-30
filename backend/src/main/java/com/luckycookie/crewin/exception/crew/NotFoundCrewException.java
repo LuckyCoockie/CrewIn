6815fc88bfb7139ae8bd330f24e3005b0e93f417
김민경 <org9899@naver.com>
2024-07-30 14:08:57 +0900

    🐛fix : 크루 존재 여부 Exception CrewId 제외

--- backend/src/main/java/com/luckycookie/crewin/exception/crew/NotFoundCrewException.java
+++ backend/src/main/java/com/luckycookie/crewin/exception/crew/NotFoundCrewException.java
@@ -4,7 +4,7 @@ import static com.luckycookie.crewin.exception.constants.CrewExceptionList.NOT_F
 
 public class NotFoundCrewException extends CrewException {
 
-    public NotFoundCrewException(Long crewId) {
+    public NotFoundCrewException() {
         super(NOT_FOUND_CREW_ERROR.getErrorCode(),
                 NOT_FOUND_CREW_ERROR.getHttpStatus(),
                 NOT_FOUND_CREW_ERROR.getMessage());
