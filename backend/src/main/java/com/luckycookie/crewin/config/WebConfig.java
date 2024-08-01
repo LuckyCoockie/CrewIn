b54a499794e49b5cb5661a87d5b9d0d5301881ad
권오윤 <rdd2033@naver.com>
2024-08-01 15:24:49 +0900

    🔧chore : http, https localhost 허용

--- backend/src/main/java/com/luckycookie/crewin/config/WebConfig.java
+++ backend/src/main/java/com/luckycookie/crewin/config/WebConfig.java
@@ -25,7 +25,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedHeaders("*")
                 .exposedHeaders("*")
-                .allowedOrigins("http://localhost:5173") // 허용할 출처
+                .allowedOrigins("https://localhost:5173", "http://localhost:5173") // 허용할 출처
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
