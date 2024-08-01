b433ad9eabea61dd279b72df38ea46b8fa42c771
김민경 <org9899@naver.com>
2024-08-01 13:26:34 +0900

    ➕feat : 세션 조회 API 생성 (내가 참가한, 만든)

--- backend/src/main/java/com/luckycookie/crewin/controller/MyPageController.java
+++ backend/src/main/java/com/luckycookie/crewin/controller/MyPageController.java
@@ -1,5 +1,6 @@
 package com.luckycookie.crewin.controller;
 
+import com.luckycookie.crewin.dto.MyPageResponse;
 import com.luckycookie.crewin.dto.MyPageResponse.MyProfileResponse;
 import com.luckycookie.crewin.dto.base.BaseResponse;
 import com.luckycookie.crewin.security.dto.CustomUser;
@@ -11,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
@@ -27,6 +29,15 @@ public class MyPageController {
         return ResponseEntity.ok(BaseResponse.create(HttpStatus.OK.value(), "내 프로필 조회를 성공했습니다.", myPageService.getMyProfile(customUser)));
     }
 
+    // 내가 만든 세션 조회
+    @GetMapping("/session")
+    public ResponseEntity<BaseResponse<MyPageResponse.MyPageSessionResponse>> getCreatedMySession(@AuthenticationPrincipal CustomUser customUser, @RequestParam int pageNo, @RequestParam String type) {
+        if(type.equals("created")) {
+            return ResponseEntity.ok(BaseResponse.create(HttpStatus.OK.value(), "내가 만든 세션 조회를 성공했습니다.", myPageService.getCreatedMySession(customUser, pageNo, type)));
+        }else {
+            return ResponseEntity.ok(BaseResponse.create(HttpStatus.OK.value(), "내가 참가한 세션 조회를 성공했습니다.", myPageService.getCreatedMySession(customUser, pageNo, type)));
+        }
+    }
 
 
 }
--- backend/src/main/java/com/luckycookie/crewin/dto/MyPageResponse.java
+++ backend/src/main/java/com/luckycookie/crewin/dto/MyPageResponse.java
@@ -1,12 +1,26 @@
 package com.luckycookie.crewin.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @NoArgsConstructor
 public class MyPageResponse {
+
+    @Getter
+    @NoArgsConstructor
+    @SuperBuilder
+    @AllArgsConstructor
+    public static class MyPageItemBaseResponse {
+        int pageNo;
+        int lastPageNo;
+    }
 
     @Getter
     @NoArgsConstructor
@@ -18,6 +32,27 @@ public class MyPageResponse {
         private int totalDistance;
         private int totalTime;
         private int totalAttendance;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @Builder
+    @AllArgsConstructor
+    public static class MyPageSessionItem{
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        private LocalDateTime startAt; // 시작 시간
+
+        private String sessionName; // 세션명
+        private String imageUrl; // 세션 포스터 이미지 (첫번째)
+        private Long sessionId; // 세션 ID
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @SuperBuilder
+    @AllArgsConstructor
+    public static class MyPageSessionResponse extends MyPageItemBaseResponse{
+        List<MyPageSessionItem> sessions;
     }
 
 }
