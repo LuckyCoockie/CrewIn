9de26572dabb78e34ca474fa89c8fa4d2dab0b6d
박효진 <jam_ty@naver.com>
2024-08-02 17:09:55 +0900

    🎨design : Floating 위치 변경

--- frontend/src/components/atoms/Button/FloatingActionButton.tsx
+++ frontend/src/components/atoms/Button/FloatingActionButton.tsx
@@ -6,14 +6,16 @@ type OwnProps = {
 
 const FloatingActionButton = ({ icon: Icon, className, onClick }: OwnProps) => {
   return (
-    <div className="sticky bottom-16 right-4 max-w-[550px] w-full flex justify-end">
-      <button
-        className={`w-[56px] h-[56px] bg-white text-white font-bold py-4 px-4 rounded-full shadow-lg focus:outline-none hover:ring-1 hover:ring-primary-500 ${className}`}
-        onClick={onClick}
-      >
-        {Icon ? <Icon /> : ""}
-      </button>
-    </div>
+    <button
+      className={`fixed w-[56px] h-[56px] bg-white text-white font-bold py-4 px-4 rounded-full shadow-lg focus:outline-none hover:ring-1 hover:ring-primary-500 ${className}`}
+      onClick={onClick}
+      style={{
+        bottom: "calc(10% + 16px)", // 5%에서 약간의 여유를 추가
+        left: "calc(70% + 16px)", // 5%에서 약간의 여유를 추가
+      }}
+    >
+      {Icon ? <Icon /> : ""}
+    </button>
   );
 };
 
