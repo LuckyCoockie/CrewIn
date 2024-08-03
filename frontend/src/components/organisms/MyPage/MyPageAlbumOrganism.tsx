a69cb5154c89db5b2ae66426befb1994a03426d9
박효진 <jam_ty@naver.com>
2024-08-03 20:25:23 +0900

    ➕feat : MyPage 내정보 API (러닝 정보, 참가세션, 만든세션, 지도목록) 연결

--- frontend/src/components/organisms/MyPage/MyPageAlbumOrganism.tsx
+++ frontend/src/components/organisms/MyPage/MyPageAlbumOrganism.tsx
@@ -1,26 +1,27 @@
 import React from "react";
-import logo from "../../../assets/images/crewinlogo.png";
+import { MyGalleryDto, getMyGallery } from "../../../apis/api/mypage";
+import InfiniteScrollComponent from "../../molecules/InfinityScrollMolecules";
 
 const MyPageAlbumOrganism: React.FC = () => {
-  const photos = [
-    logo,
-    logo,
-    logo,
-    logo,
-    logo,
-    logo,
-    logo,
-    logo,
-    logo,
-    logo,
-    logo,
-  ];
+  const fetchGallery = async (page: number): Promise<MyGalleryDto[]> => {
+    const response = await getMyGallery(page);
+    return response.postGalleryList;
+  };
+
   return (
-    <>
-      {photos.map((photo, index) => (
-        <img src={photo} alt="photo" key={index} className="w-1/3 h-1/3" />
-      ))}
-    </>
+    <InfiniteScrollComponent
+      fetchKey="myGallery"
+      fetchData={fetchGallery}
+      ItemComponent={({ data }) => (
+        <img
+          src={data.thumbnailImage}
+          alt={`gallery-item-${data.postId}`}
+          className="w-1/3 h-1/3"
+        />
+      )}
+      className="grid grid-cols-3 gap-4"
+      pageSize={10}
+    />
   );
 };
 
