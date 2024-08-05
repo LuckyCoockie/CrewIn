b225e33fdcb7d4d0d344b6ee7ef4e16247181642
박효진 <jam_ty@naver.com>
2024-08-06 02:48:50 +0900

    📦️refactor : api 호출 (organism -> template) 이동 후 prop

--- frontend/src/components/templates/MyProfileTemplate.tsx
+++ frontend/src/components/templates/MyProfileTemplate.tsx
@@ -12,9 +12,11 @@ import {
   ProfileDto,
   getMyMaps,
   MyMapsDto,
+  getMyMadeSessions,
+  getMyParticipatedSessions,
+  MyMadeSessionDto,
+  MyParticipatedSessionDto,
 } from "../../apis/api/mypage";
-
-import { useParams } from "react-router";
 
 const MyProfileTemplate: React.FC = () => {
   console.log("여기는 내페이지");
@@ -24,16 +26,15 @@ const MyProfileTemplate: React.FC = () => {
   };
 
   const texts = ["러닝 정보", "사진첩"];
-  const { memberId } = useParams<{ memberId: string }>();
-  const numericMemberId = memberId ? Number(memberId) : null;
+  const numericMemberId = null;
 
-  // React Query를 사용하여 데이터를 가져옴
+  // React Query를 사용하여 데이터를 한 번에 가져옴
   const {
     data: profileData,
     isLoading: isProfileLoading,
     isError: isProfileError,
   } = useQuery<ProfileDto>(["myProfile", numericMemberId], () =>
-    getMyProfileInfo(numericMemberId)
+    getMyProfileInfo()
   );
 
   const {
@@ -42,19 +43,40 @@ const MyProfileTemplate: React.FC = () => {
     isError: isMapsError,
   } = useQuery<MyMapsDto[]>("myMaps", getMyMaps);
 
+  const {
+    data: madeSessions,
+    isLoading: isMadeSessionsLoading,
+    isError: isMadeSessionsError,
+  } = useQuery<MyMadeSessionDto[]>(["myMadeSessions", 0], () =>
+    getMyMadeSessions(0).then((res) => res.items)
+  );
+
+  const {
+    data: participatedSessions,
+    isLoading: isParticipatedSessionsLoading,
+    isError: isParticipatedSessionsError,
+  } = useQuery<MyParticipatedSessionDto[]>(["myParticipatedSessions", 0], () =>
+    getMyParticipatedSessions(0).then((res) => res.items)
+  );
+
   // 로딩 상태 처리
-  if (isProfileLoading || isMapsLoading) {
+  if (
+    isProfileLoading ||
+    isMapsLoading ||
+    isMadeSessionsLoading ||
+    isParticipatedSessionsLoading
+  ) {
     return <div>Loading...</div>;
   }
 
   // 에러 상태 처리
-  if (isProfileError || isMapsError) {
+  if (
+    isProfileError ||
+    isMapsError ||
+    isMadeSessionsError ||
+    isParticipatedSessionsError
+  ) {
     return <div>Error loading data</div>;
-  }
-
-  // 데이터가 존재하지 않을 경우의 처리
-  if (!profileData || !mapsData) {
-    return <div>No data available</div>;
   }
 
   return (
@@ -62,7 +84,7 @@ const MyProfileTemplate: React.FC = () => {
       <header>
         <MyPageHeaderOrganism />
       </header>
-      <MyPageRecordInfoOrganism profileData={profileData} />
+      <MyPageRecordInfoOrganism profileData={profileData!} />
       <NavTabMolecule
         texts={texts}
         onTabClick={handleTabClick}
@@ -71,13 +93,15 @@ const MyProfileTemplate: React.FC = () => {
       {currentTab === "러닝 정보" ? (
         <>
           <div className="my-4">
-            <MyPageMadeSessionOrganism />
+            <MyPageMadeSessionOrganism sessions={madeSessions!} />
           </div>
           <div className="my-4">
-            <MyPageParticipatedSessionOrganism />
+            <MyPageParticipatedSessionOrganism
+              sessions={participatedSessions!}
+            />
           </div>
           <div className="my-4">
-            <MyPageMapOrganism mapsData={mapsData} />
+            <MyPageMapOrganism mapsData={mapsData!} />
           </div>
         </>
       ) : (
