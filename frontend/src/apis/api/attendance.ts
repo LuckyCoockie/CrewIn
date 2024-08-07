75233522cb1047f3053817e6727c1fb2a8970e35
박준식 <akfldkskgorn@gmail.com>
2024-08-07 17:20:25 +0900

    ➕feat : Attendance Organism 구현

---
+++ frontend/src/apis/api/attendance.ts
@@ -0,0 +1,87 @@
+import api from "../index";
+
+export type AttendanceMemberDto = {
+  memberSessionId: number;
+  name: string;
+  nickname: string;
+  imageUrl: string;
+};
+
+export type GetAttendanceMemberListRequestDto = {
+  sessionId: number;
+};
+
+export const getAttendanceMemberList = async (
+  dto: GetAttendanceMemberListRequestDto
+): Promise<AttendanceMemberDto[]> => {
+  return [
+    {
+      memberSessionId: 1,
+      name: "name",
+      nickname: "nickname",
+      imageUrl:
+        "https://crewin-bucket.s3.ap-northeast-2.amazonaws.com/crewin/crewinlogo.webp",
+    },
+    {
+      memberSessionId: 1,
+      name: "name",
+      nickname: "nickname",
+      imageUrl:
+        "https://crewin-bucket.s3.ap-northeast-2.amazonaws.com/crewin/crewinlogo.webp",
+    },
+    {
+      memberSessionId: 1,
+      name: "name",
+      nickname: "nickname",
+      imageUrl:
+        "https://crewin-bucket.s3.ap-northeast-2.amazonaws.com/crewin/crewinlogo.webp",
+    },
+    {
+      memberSessionId: 1,
+      name: "name",
+      nickname: "nickname",
+      imageUrl:
+        "https://crewin-bucket.s3.ap-northeast-2.amazonaws.com/crewin/crewinlogo.webp",
+    },
+    {
+      memberSessionId: 1,
+      name: "name",
+      nickname: "nickname",
+      imageUrl:
+        "https://crewin-bucket.s3.ap-northeast-2.amazonaws.com/crewin/crewinlogo.webp",
+    },
+  ];
+  const response = await api.get<AttendanceMemberDto[]>(
+    `/attendence/member/${dto.sessionId}`
+  );
+  return response.data;
+};
+
+export type StartAttendanceRequestDto = {
+  sessionId: number;
+};
+
+export const startAttendance = async (
+  dto: GetAttendanceMemberListRequestDto
+) => {
+  await api.post(`/attendance/start/${dto.sessionId}`);
+};
+
+export type ChangeAttendRequestDto = {
+  memberSessionId: number;
+  attend: boolean;
+};
+
+export const changeAttend = async (dto: ChangeAttendRequestDto) => {
+  await api.put(`/attendance/${dto.memberSessionId}`, null, {
+    params: { attend: dto.attend },
+  });
+};
+
+export type PostAttendRequestDto = {
+  sessionId: number;
+};
+
+export const postAttend = async (dto: PostAttendRequestDto) => {
+  await api.post(`/attendance/guest/${dto.sessionId}`);
+};
