b731027a61e1e39bcfa3559f333b9e68ffe7116c
박효진 <jam_ty@naver.com>
2024-08-07 14:13:47 +0900

    🎨design : 가독성, 가시성 향상

--- frontend/src/components/atoms/Button/IntoArrowButton.tsx
+++ frontend/src/components/atoms/Button/IntoArrowButton.tsx
@@ -3,15 +3,20 @@ import { ReactComponent as IntoArrowIcon } from "../../../assets/icons/intoarrow
 import { useNavigate } from "react-router";
 
 type Props = {
-  router: string
+  router: string;
 };
 
 const IntoArrowButton: React.FC<Props> = ({ router }) => {
-    const navigate = useNavigate()
-    const moveRouter = () => {
-        navigate(router)
-    }
-  return <IntoArrowIcon onClick={moveRouter} className="cursor-pointer"/>;
+  const navigate = useNavigate();
+  const moveRouter = () => {
+    navigate(router);
+  };
+  return (
+    <IntoArrowIcon
+      onClick={moveRouter}
+      className="ms-2 cursor-pointer rounded-full bg-gray-50"
+    />
+  );
 };
 
 export default IntoArrowButton;
