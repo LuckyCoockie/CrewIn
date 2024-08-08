50e4360947b813d7fe4d4e2088c99228739b2515
김민경 <org9899@naver.com>
2024-08-08 10:58:58 +0900

    🐛fix : 내가 만든 코스 목록 조회 API 수정 (ID 기준 역순 정렬)

--- backend/src/main/java/com/luckycookie/crewin/repository/CourseRepository.java
+++ backend/src/main/java/com/luckycookie/crewin/repository/CourseRepository.java
@@ -9,5 +9,5 @@ import java.util.List;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
-    List<Course> findByCreator(Member creator);
+    List<Course> findByCreatorOrderByIdDesc(Member creator);
 }
