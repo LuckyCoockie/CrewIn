db5de1f76b667da8525018711cc43dd2609800be
박효진 <jam_ty@naver.com>
2024-08-09 23:58:44 +0900

    🎨design : 글자 색 수정, text 데이터 사용

--- frontend/src/components/atoms/Input/InputBannermageComponent.tsx
+++ frontend/src/components/atoms/Input/InputBannermageComponent.tsx
@@ -6,98 +6,99 @@ type InputImage = {
   name: string;
   placeholder: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  previewUrl?: string; // 추가
+  previewUrl?: string; 
 };
 
-const InputBannermageComponent = React.forwardRef<
-  HTMLInputElement,
-  InputImage
->((props, ref) => {
-  const [preview, setPreview] = useState<string | null>(props.previewUrl || null);
-  const [fileCount, setFileCount] = useState(0);
+const InputBannermageComponent = React.forwardRef<HTMLInputElement, InputImage>(
+  (props, ref) => {
+    const [preview, setPreview] = useState<string | null>(
+      props.previewUrl || null
+    );
+    const [fileCount, setFileCount] = useState(0);
 
-  useEffect(() => {
-    if (props.previewUrl) {
-      setPreview(props.previewUrl);
-    }
-  }, [props.previewUrl]);
+    useEffect(() => {
+      if (props.previewUrl) {
+        setPreview(props.previewUrl);
+      }
+    }, [props.previewUrl]);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const files = e.target.files;
-    setFileCount(files ? files.length : 0);
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const files = e.target.files;
+      setFileCount(files ? files.length : 0);
 
-    if (files && files[0]) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setPreview(reader.result as string);
-      };
-      reader.readAsDataURL(files[0]);
-    } else {
-      setPreview(null);
-    }
-    props.onChange(e);
-  };
+      if (files && files[0]) {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          setPreview(reader.result as string);
+        };
+        reader.readAsDataURL(files[0]);
+      } else {
+        setPreview(null);
+      }
+      props.onChange(e);
+    };
 
-  return (
-    <>
-      <div className="w-full text-center mb-3">
-        <label htmlFor={props.id} className="w-full">
-          <div className="flex p-2 border border-gray-300 rounded-lg w-full">
-            <p className="ml-1">🔗</p>
-            {fileCount > 0 ? (
-              <p className="text-gray-600 font-semibold ml-1">
-                이미지가 선택되었습니다.
-              </p>
-            ) : (
-              <p className="text-gray-600 font-semibold ml-1">
-                크루 상세페이지에 노출됩니다.
-              </p>
-            )}
-          </div>
-        </label>
-        <input
-          className="hidden"
-          id={props.id}
-          type="file"
-          name={props.name}
-          placeholder={props.placeholder}
-          onChange={handleChange}
-          ref={ref}
-          accept="image/*"
-        />
-      </div>
-      {preview ? (
-        <>
-          <div className="w-full text-center">
-            <img
-              src={preview}
-              alt="Preview"
-              className="border-2 w-60 h-40 object-cover"
-            />
-            <p className="mt-2 text-sm font-medium text-gray-500">
-              배너 노출 예시
-            </p>
-          </div>
-        </>
-      ) : (
-        <>
-          <div className="w-full text-center">
-            <img
-              src={crewinbanner}
-              alt="crewinbanner"
-              className="border-2 w-60 h-40 object-cover"
-            />
-          </div>
-          <label
-            htmlFor={props.id}
-            className="text-center block mt-2 text-sm font-medium text-gray-500 dark:text-white"
-          >
-            *{props.placeholder}
+    return (
+      <>
+        <div className="w-full text-center mb-3">
+          <label htmlFor={props.id} className="w-full">
+            <div className="flex p-2 border border-gray-300 rounded-lg w-full">
+              <p className="ml-1">🔗</p>
+              {fileCount > 0 ? (
+                <p className="text-gray-500 font-semibold ml-1">
+                  이미지가 선택되었습니다.
+                </p>
+              ) : (
+                <p className="text-gray-500 font-semibold ml-1">
+                  3:2 비율이 가장 적합합니다.
+                </p>
+              )}
+            </div>
           </label>
-        </>
-      )}
-    </>
-  );
-});
+          <input
+            className="hidden"
+            id={props.id}
+            type="file"
+            name={props.name}
+            placeholder={props.placeholder}
+            onChange={handleChange}
+            ref={ref}
+            accept="image/*"
+          />
+        </div>
+        {preview ? (
+          <>
+            <div className="w-full text-center">
+              <img
+                src={preview}
+                alt="Preview"
+                className="border-2 w-60 h-40 object-cover"
+              />
+              <p className="mt-2 text-sm font-medium text-gray-500">
+                배너 노출 예시
+              </p>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="w-full text-center">
+              <img
+                src={crewinbanner}
+                alt="crewinbanner"
+                className="border-2 w-60 h-40 object-cover"
+              />
+            </div>
+            <label
+              htmlFor={props.id}
+              className="text-center block mt-2 text-sm font-medium text-gray-500 dark:text-white"
+            >
+              *{props.placeholder}
+            </label>
+          </>
+        )}
+      </>
+    );
+  }
+);
 
 export default InputBannermageComponent;
