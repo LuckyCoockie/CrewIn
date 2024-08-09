1f77fad5185232c0051112adac441a58aeefa6d9
권오윤 <rdd2033@naver.com>
2024-08-10 00:22:35 +0900

    🐛fix : SSE 이벤트네임 변경

--- backend/src/main/java/com/luckycookie/crewin/exception/constants/HeartExceptionList.java
+++ backend/src/main/java/com/luckycookie/crewin/exception/constants/HeartExceptionList.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum HeartExceptionList {
     NOT_FOUND_HEART_ERROR("H0001", HttpStatus.NOT_FOUND, "좋아요를 찾는데 실패했습니다."),
-    ALREADY_EXSIST_HEART_ERROR("H0001", HttpStatus.NOT_FOUND, "이미 좋아요가 존재합니다.")
+    ALREADY_EXIST_HEART_ERROR("H0001", HttpStatus.NOT_FOUND, "이미 좋아요가 존재합니다.")
     ;
 
     private final String errorCode;
