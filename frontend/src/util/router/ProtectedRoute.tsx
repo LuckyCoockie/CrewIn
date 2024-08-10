aad2ab5c3654a8ea8f72b2a48c7e0bbb8dfefe45
박준식 <akfldkskgorn@gmail.com>
2024-08-10 18:29:38 +0900

    🐛fix : 리프레쉬 토큰 무한 재요청 버그 수정

--- frontend/src/util/router/ProtectedRoute.tsx
+++ frontend/src/util/router/ProtectedRoute.tsx
@@ -2,7 +2,7 @@ import { useEffect, useMemo } from "react";
 import { useSelector } from "react-redux";
 import { Outlet, useNavigate } from "react-router-dom";
 import { RootState } from "../../modules";
-// import { refreshToken } from "../../apis/api/authorization";
+import { refreshToken } from "../../apis/api/authorization";
 
 const ProtectedRoute = () => {
   const { accessToken, loading } = useSelector(
@@ -14,11 +14,12 @@ const ProtectedRoute = () => {
   const isAuthenticated = useMemo(() => accessToken !== null, [accessToken]);
 
   useEffect(() => {
-    // if (!loading && !isAuthenticated) {
-    //   refreshToken().catch(() => navigate("/login"));
-    // }
+    if (!loading && !isAuthenticated) {
+      refreshToken().catch(() => navigate("/login"));
+    }
   }, [isAuthenticated, loading, navigate]);
 
+  if (!isAuthenticated) return <>스플레쉬</>;
   return <Outlet />;
 };
 
