8f69389f1cb1ff2792e18c9e6245bcae572a6d12
박효진 <jam_ty@naver.com>
2024-08-11 14:19:33 +0900

    ➕feat : 상황 별 modal 추가

--- frontend/src/pages/ProfileInfoPage.tsx
+++ frontend/src/pages/ProfileInfoPage.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { getMyProfileInfo } from "../apis/api/mypage";
 import { uploadImage } from "../apis/api/presigned";
 import {
@@ -5,11 +6,19 @@ import {
   editNickname,
   editPassword,
 } from "../apis/api/profile";
+import Modal from "../components/molecules/ModalMolecules";
 import { ProfileInfoTemplate } from "../components/templates/profile/ProfileInfoTemplate";
 import { useNavigate } from "react-router";
 
 export const ProfileInfoPage = () => {
   const navigate = useNavigate();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
+
   const onProfileImageEdit = async (
     { image }: { image?: File },
     onClose: () => void
@@ -23,6 +32,10 @@ export const ProfileInfoPage = () => {
         navigate(`/profile`, { replace: true });
       }
     } catch (error) {
+      setModalMessage(
+        "프로필 이미지 업로드에 실패했습니다. 잠시 후 다시 시도해주세요."
+      );
+      setIsModalOpen(true);
       console.error("프로필 이미지 업로드 에러:", error);
     }
   };
@@ -37,7 +50,9 @@ export const ProfileInfoPage = () => {
       onClose(); // 성공적으로 변경 후 모달 닫기
       navigate(`/profile`, { replace: true });
     } catch (error) {
-      console.error(error);
+      setModalMessage("닉네임 변경에 실패했습니다. 아이디를 확인하세요.");
+      setIsModalOpen(true);
+      console.error("닉네임 변경 에러:", error);
     }
   };
 
@@ -51,17 +66,27 @@ export const ProfileInfoPage = () => {
       onClose(); // 성공적으로 변경 후 모달 닫기
       navigate(`/profile`, { replace: true });
     } catch (error) {
+      setModalMessage(
+        "비밀번호 변경에 실패했습니다. 이전 비밀번호를 확인하세요."
+      );
+      setIsModalOpen(true);
       console.error("비밀번호 변경 에러", error);
-      window.alert("이전 비밀번호를 확인하세요.");
     }
   };
 
   return (
-    <ProfileInfoTemplate
-      fetchData={getMyProfileInfo}
-      onProfileImageEdit={onProfileImageEdit}
-      onNicknameEdit={onNicknameEdit}
-      onPasswordEdit={onPasswordEdit}
-    />
+    <>
+      <ProfileInfoTemplate
+        fetchData={getMyProfileInfo}
+        onProfileImageEdit={onProfileImageEdit}
+        onNicknameEdit={onNicknameEdit}
+        onPasswordEdit={onPasswordEdit}
+      />
+      {isModalOpen && (
+        <Modal title="오류" onClose={handleModalClose}>
+          <p>{modalMessage}</p>
+        </Modal>
+      )}
+    </>
   );
 };
