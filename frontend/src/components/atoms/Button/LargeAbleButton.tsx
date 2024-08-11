47dd4b6171acbfc8bc600306a6e49f6acdf9cd7d
박효진 <jam_ty@naver.com>
2024-08-11 12:32:07 +0900

    ➕feat : 버튼 disable prop 전달 정보 추가

--- frontend/src/components/atoms/Button/LargeAbleButton.tsx
+++ frontend/src/components/atoms/Button/LargeAbleButton.tsx
@@ -3,6 +3,7 @@ import React from "react";
 type Text = {
   onClick?: () => void;
   text: string;
+  isLoading?: boolean;
 };
 
 const LargeAbleButton: React.FC<Text> = (props) => {
@@ -10,6 +11,7 @@ const LargeAbleButton: React.FC<Text> = (props) => {
     <button
       className="w-full bg-[#2B2F40] py-4 px-8 text-center rounded-lg text-white font-bold"
       onClick={props.onClick}
+      disabled={props.isLoading}
     >
       {props.text}
     </button>
