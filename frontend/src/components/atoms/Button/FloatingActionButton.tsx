e52816ed17303c721e8ad14ae1638efa166c17df
박효진 <jam_ty@naver.com>
2024-08-11 20:44:40 +0900

    🐛fix : 플로팅 버튼 버그 fix, 세션 이미지 추가 시 z-index 해결

--- frontend/src/components/atoms/Button/FloatingActionButton.tsx
+++ frontend/src/components/atoms/Button/FloatingActionButton.tsx
@@ -12,9 +12,9 @@ const FloatingActionButton: React.FC<OwnProps> = ({
   children,
 }) => {
   return (
-    <div className="fixed bottom-24 left-0 pr-10 right-12 flex justify-end max-w-[550px] w-full mx-auto disabled">
+    <div className="z-50 fixed bottom-24 left-0 pr-10 right-12 flex justify-end max-w-[550px] w-full mx-auto disabled">
       <button
-        className={`z-50 w-[56px] h-[56px] bg-white text-white font-bold py-4 px-4 rounded-full shadow-lg focus:outline-none hover:ring-1 hover:ring-primary-500 ${className}`}
+        className={`w-[56px] h-[56px] bg-white text-white font-bold py-4 px-4 rounded-full shadow-lg focus:outline-none hover:ring-1 hover:ring-primary-500 ${className}`}
         onClick={onClick}
         style={{ pointerEvents: "auto" }}
       >
