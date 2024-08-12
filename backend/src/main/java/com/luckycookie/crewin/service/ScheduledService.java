9e42b9d18252d77072614ace15d79a30d508d93f
권오윤 <rdd2033@naver.com>
2024-08-12 14:11:37 +0900

    🐛fix : 스케줄링에서 크루 프록시 있나 체크하는 로깅 추가

--- backend/src/main/java/com/luckycookie/crewin/service/ScheduledService.java
+++ backend/src/main/java/com/luckycookie/crewin/service/ScheduledService.java
@@ -35,32 +35,28 @@ public class ScheduledService {
     @Async
     public void closeAttendance(Session session) {
         log.info("Session{}: attendance check start", session.getId());
+        // 멤버 세션 테이블에서 모두 가져와서 한명씩 출석 반영 수행
+        List<MemberSession> attendanceList = memberSessionRepository.findBySession(session);
 
-        try {
-            // 멤버 세션 테이블에서 모두 가져와서 한명씩 출석 반영 수행
-            List<MemberSession> attendanceList = memberSessionRepository.findBySession(session);
+        for (MemberSession memberSession : attendanceList) {
+            if (!memberSession.getIsAttend())
+                continue;
 
-            for (MemberSession memberSession : attendanceList) {
-                if (!memberSession.getIsAttend())
-                    continue;
-
-                // 만약 오픈런이면 멤버크루에 없으면 올려주기(false, false)
-                if (session.getSessionType() == SessionType.OPEN && !memberCrewRepository.existsByMemberAndCrew(memberSession.getMember(), session.getCrew())) {
-                    memberCrewRepository.save(MemberCrew.builder().member(memberSession.getMember()).crew(session.getCrew()).build());
-                }
-
-                // 회원테이블에 참여횟수, 누적거리, 누적시간 반영
-                Member member = memberRepository.findById(memberSession.getMember().getId()).orElseThrow(NotFoundMemberException::new);
-                member.updateRunRecord(session);
-
-                // 정규런, 오픈런인 경우 멤버크루에 참가 횟수 반영
-                if (session.getSessionType() != THUNDER) {
-                    MemberCrew memberCrew = memberCrewRepository.findByMemberAndCrew(member, session.getCrew()).orElseThrow(NotFoundMemberSessionException::new);
-                    memberCrew.plusAttendance();
-                }
+            // 만약 오픈런이면 멤버크루에 없으면 올려주기(false, false)
+            if (session.getSessionType() == SessionType.OPEN && !memberCrewRepository.existsByMemberAndCrew(memberSession.getMember(), session.getCrew())) {
+                memberCrewRepository.save(MemberCrew.builder().member(memberSession.getMember()).crew(session.getCrew()).build());
             }
-        } catch (Exception e) {
-            log.error("Scheduled Error!!! {}", e.getMessage());
+
+            // 회원테이블에 참여횟수, 누적거리, 누적시간 반영
+            Member member = memberRepository.findById(memberSession.getMember().getId()).orElseThrow(NotFoundMemberException::new);
+            member.updateRunRecord(session);
+
+            // 정규런, 오픈런인 경우 멤버크루에 참가 횟수 반영
+            if (session.getSessionType() != THUNDER) {
+                MemberCrew memberCrew = memberCrewRepository.findByMemberAndCrew(member, session.getCrew()).orElseThrow(NotFoundMemberSessionException::new);
+                log.info("crew exist@@ : {}", session.getCrew());
+                memberCrew.plusAttendance();
+            }
         }
     }
 }
