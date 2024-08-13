4976a299c33f04a45c4f8bec6b7400971950f233
권오윤 <rdd2033@naver.com>
2024-08-13 17:21:19 +0900

    🐛fix : 토큰 검증 컨트롤러 추가

--- backend/src/main/java/com/luckycookie/crewin/config/SecurityConfig.java
+++ backend/src/main/java/com/luckycookie/crewin/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
                             .requestMatchers("/member/email").permitAll() // 이메일(id) 찾기
                             .requestMatchers(HttpMethod.POST, "/member/password").permitAll() // 임시 비밀번호 발급만 접근 허용
                             .requestMatchers("/attendance/**").permitAll()
-                            .requestMatchers("/health").permitAll() // /health 엔드포인트 접근 허용
+                            .requestMatchers("/health/**").permitAll() // /health 엔드포인트 접근 허용
                             .anyRequest().authenticated();
                 });
 
--- backend/src/main/java/com/luckycookie/crewin/controller/HealthController.java
+++ backend/src/main/java/com/luckycookie/crewin/controller/HealthController.java
@@ -1,8 +1,12 @@
 package com.luckycookie.crewin.controller;
 
+import com.luckycookie.crewin.security.util.TokenUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/health")
 @RequiredArgsConstructor
 public class HealthController {
+    private final TokenUtil tokenUtil;
+
     @GetMapping()
     public ResponseEntity<String> health() {
+        return ResponseEntity.ok("OK");
+    }
+
+    @GetMapping("/token")
+    public ResponseEntity<String> tokenValidation(@RequestHeader("Authorization") String token, HttpServletRequest request) {
+        if (!tokenUtil.validateToken(token.substring(7), request)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid token");
+        }
         return ResponseEntity.ok("OK");
     }
 }
