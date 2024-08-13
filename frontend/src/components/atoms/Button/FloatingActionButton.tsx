34ba6e5f3c77aaf13e7c1aeb4e828aaa9235b8c4
박준식 <akfldkskgorn@gmail.com>
2024-08-13 11:00:51 +0900

    🎨design : fab 반응형 padding 추가

--- frontend/src/components/atoms/Button/FloatingActionButton.tsx
+++ frontend/src/components/atoms/Button/FloatingActionButton.tsx
@@ -12,15 +12,21 @@ const FloatingActionButton: React.FC<OwnProps> = ({
   children,
 }) => {
   return (
-    <div className="z-50 fixed bottom-24 md:bottom-8 left-0 pr-4 right-8 flex justify-end max-w-[500px] w-full mx-auto disabled">
-      <button
-        className={`w-[56px] h-[56px] bg-white text-white font-bold py-4 px-4 rounded-full shadow-lg focus:outline-none hover:ring-1 hover:ring-primary-500 ${className}`}
-        onClick={onClick}
-        style={{ pointerEvents: "auto" }}
-      >
-        {children}
-      </button>
-    </div>
+    <button
+      className={`
+        fab
+        fixed bottom-20 right-4 md:bottom-10 md:right-10
+        w-14 h-14 bg-white rounded-full shadow-xl
+        ring-1 ring-gray-100 hover:ring-primary-500
+        focus:outline-none transition duration-300 ease-in-out
+        flex items-center justify-center
+        ${className}
+      `}
+      onClick={onClick}
+      style={{ pointerEvents: "auto" }}
+    >
+      {children}
+    </button>
   );
 };
 
