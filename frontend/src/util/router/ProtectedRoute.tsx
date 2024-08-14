d660264c6736314bf05bc05fc93da60d26715b9e
박준식 <akfldkskgorn@gmail.com>
2024-08-15 01:00:01 +0900

    🐛fix : protected routing refresh 로직 삭제

--- frontend/src/util/router/ProtectedRoute.tsx
+++ frontend/src/util/router/ProtectedRoute.tsx
@@ -2,7 +2,7 @@ import { useEffect, useMemo } from "react";
 import { useSelector } from "react-redux";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { RootState } from "../../modules";
-import { refreshToken } from "../../apis/api/authorization";
+// import { refreshToken } from "../../apis/api/authorization";
 import SplashPage from "./SplashPage";
 import BottomBarOrganism from "../../components/organisms/BottomBarOrganism";
 import useIsMobile from "../windowSize/useIsMobile";
@@ -18,11 +18,11 @@ const ProtectedRoute = () => {
   const isAuthenticated = useMemo(() => accessToken !== null, [accessToken]);
 
   useEffect(() => {
-    if (!loading && !isAuthenticated) {
-      refreshToken().catch(() =>
-        navigate("/login", { state: { navigateFrom: location }, replace: true })
-      );
-    }
+    // if (!loading && !isAuthenticated) {
+    //   refreshToken().catch(() =>
+    //     navigate("/login", { state: { navigateFrom: location }, replace: true })
+    //   );
+    // }
   }, [isAuthenticated, loading, location, navigate]);
 
   // 바텀바를 그릴 페이지
