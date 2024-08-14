09a047ab422d81fe19211f476e5953e86e0def34
박준식 <akfldkskgorn@gmail.com>
2024-08-15 01:53:05 +0900

    🐛fix : auth delay 삭제

--- frontend/src/util/auth.ts
+++ frontend/src/util/auth.ts
@@ -8,35 +8,34 @@ import store from "../modules";
 import { clearAccessToken, setAccessToken } from "../modules/reducers/auth";
 import { debounce } from "lodash";
 
-export const setAuth = debounce(
-  (dto: { accessToken: string; memberId: number }) => {
-    const currentTime = new Date().getTime().toString();
-    localStorage.setItem("AUTH", currentTime);
-    store.dispatch(setAccessToken(dto.accessToken, dto.memberId));
-    store.dispatch(
-      addInterceptor(
-        "auth",
-        setTokenInterceptors(dto.accessToken),
-        clearTokenInterceptors
-      )
-    );
-    store.dispatch(
-      addInterceptor(
-        "tmap",
-        Tmap.setTokenInterceptors(dto.accessToken),
-        Tmap.clearTokenInterceptors
-      )
-    );
-  },
-  300
-);
+export const setAuth = (dto: { accessToken: string; memberId: number }) => {
+  console.log("setAuth");
+  const currentTime = new Date().getTime().toString();
+  localStorage.setItem("AUTH", currentTime);
+  store.dispatch(setAccessToken(dto.accessToken, dto.memberId));
+  store.dispatch(
+    addInterceptor(
+      "auth",
+      setTokenInterceptors(dto.accessToken),
+      clearTokenInterceptors
+    )
+  );
+  store.dispatch(
+    addInterceptor(
+      "tmap",
+      Tmap.setTokenInterceptors(dto.accessToken),
+      Tmap.clearTokenInterceptors
+    )
+  );
+};
 
-export const clearAuth = debounce((error?: string) => {
+export const clearAuth = (error?: string) => {
+  console.log("clearAuth");
   localStorage.removeItem("AUTH");
   store.dispatch(clearAccessToken(error));
   store.dispatch(removeInterceptor("auth", clearTokenInterceptors));
   store.dispatch(removeInterceptor("tmap", Tmap.clearTokenInterceptors));
-}, 300);
+};
 
 export const checkAuth = (): boolean => {
   if (!localStorage.getItem("AUTH")) return false;
