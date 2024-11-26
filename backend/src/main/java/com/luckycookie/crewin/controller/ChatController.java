f0e50523b8c5eee737630d4f10d5c869e66f42d0
권오윤 <rdd2033@naver.com>
2024-11-26 17:30:38 +0900

    chore: 컨트롤러 작성중

--- backend/src/main/java/com/luckycookie/crewin/controller/ChatController.java
+++ backend/src/main/java/com/luckycookie/crewin/controller/ChatController.java
@@ -2,11 +2,14 @@ package com.luckycookie.crewin.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @Controller
 public class ChatController {
-    
+
+    @MessageMapping("/send/{crewId}")
+    @SendTo("/topic/chat/{crewId}")
+    public
 }
